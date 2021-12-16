108c3feae0b181cb116763ab52dc102b507cb341
Al3xCubed <al3xcubed@gmail.com>
2021-12-16 21:06:51 +0100

    Day 16, Part 2

--- Day16.ts
+++ Day16.ts
@@ -22,29 +22,76 @@ const hexToBin: { [hex: string]: string } = {
 
 abstract class APacketReader {
 	public abstract isPacketType(type: number): boolean;
-	public abstract read(reader: Reader, packetVersion: number): APacket;
+	public abstract read(reader: Reader, packetVersion: number, packetTypeId: number): APacket;
 }
 
 abstract class APacket {
 	public constructor(public readonly packetVersion: number) {}
+
+	public abstract evaluate(): number;
 }
 
-class OperatorPacket extends APacket {
+abstract class AOperatorPacket extends APacket {
 	public constructor(packetVersion: number, public readonly subpackets: APacket[]) {
 		super(packetVersion);
 	}
 }
 
+class SumOperatorPacket extends AOperatorPacket {
+	public evaluate(): number {
+		return this.subpackets.map((subpacket) => subpacket.evaluate()).sum();
+	}
+}
+
+class ProductOperatorPacket extends AOperatorPacket {
+	public evaluate(): number {
+		return this.subpackets.map((subpacket) => subpacket.evaluate()).product();
+	}
+}
+
+class MinimumOperatorPacket extends AOperatorPacket {
+	public evaluate(): number {
+		return Math.min(...this.subpackets.map((subpacket) => subpacket.evaluate()));
+	}
+}
+
+class MaximumOperatorPacket extends AOperatorPacket {
+	public evaluate(): number {
+		return Math.max(...this.subpackets.map((subpacket) => subpacket.evaluate()));
+	}
+}
+
+class GreaterThanOperatorPacket extends AOperatorPacket {
+	public evaluate(): number {
+		if (this.subpackets[0].evaluate() > this.subpackets[1].evaluate()) return 1;
+		else return 0;
+	}
+}
+
+class LessThanOperatorPacket extends AOperatorPacket {
+	public evaluate(): number {
+		if (this.subpackets[0].evaluate() < this.subpackets[1].evaluate()) return 1;
+		else return 0;
+	}
+}
+
+class EqualToOperatorPacket extends AOperatorPacket {
+	public evaluate(): number {
+		if (this.subpackets[0].evaluate() === this.subpackets[1].evaluate()) return 1;
+		else return 0;
+	}
+}
+
 class OperatorPacketReader extends APacketReader {
-	public read(reader: Reader, packetVersion: number): OperatorPacket {
+	public read(reader: Reader, packetVersion: number, packetTypeId: number): AOperatorPacket {
 		const lengthTypeID = reader.readInt(1);
-		const subPackets: APacket[] = [];
+		const subpackets: APacket[] = [];
 		if (lengthTypeID === 0) {
 			const totalLengthInBits = reader.readInt(15);
 			let bitsRead = 0;
 			while (bitsRead < totalLengthInBits) {
 				const startPos = reader.pointer;
-				subPackets.push(reader.readPacket());
+				subpackets.push(reader.readPacket());
 				const endPos = reader.pointer;
 				const length = endPos - startPos;
 				bitsRead += length;
@@ -52,11 +99,28 @@ class OperatorPacketReader extends APacketReader {
 		} else {
 			const numberOfSubPackets = reader.readInt(11);
 			for (let i = 0; i < numberOfSubPackets; i++) {
-				subPackets.push(reader.readPacket());
+				subpackets.push(reader.readPacket());
 			}
 		}
 
-		return new OperatorPacket(packetVersion, subPackets);
+		switch (packetTypeId) {
+			case 0:
+				return new SumOperatorPacket(packetVersion, subpackets);
+			case 1:
+				return new ProductOperatorPacket(packetVersion, subpackets);
+			case 2:
+				return new MinimumOperatorPacket(packetVersion, subpackets);
+			case 3:
+				return new MaximumOperatorPacket(packetVersion, subpackets);
+			case 5:
+				return new GreaterThanOperatorPacket(packetVersion, subpackets);
+			case 6:
+				return new LessThanOperatorPacket(packetVersion, subpackets);
+			case 7:
+				return new EqualToOperatorPacket(packetVersion, subpackets);
+			default:
+				throw new Error(`Packet type id '${packetTypeId}' is unknown`);
+		}
 	}
 
 	public isPacketType(type: number): boolean {
@@ -68,10 +132,14 @@ class LiteralValuePacket extends APacket {
 	public constructor(packetVersion: number, public readonly value: number) {
 		super(packetVersion);
 	}
+
+	public evaluate(): number {
+		return this.value;
+	}
 }
 
 class LiteralValuePacketReader extends APacketReader {
-	public read(reader: Reader, packetVersion: number): LiteralValuePacket {
+	public read(reader: Reader, packetVersion: number, packetTypeId: number): LiteralValuePacket {
 		let valueBits = "";
 		while (true) {
 			let bitsPart = reader.read(5);
@@ -105,7 +173,7 @@ class Reader {
 		const packetTypeId = this.readInt(3);
 		return Reader.readers
 			.find((reader) => reader.isPacketType(packetTypeId))!
-			.read(this, packetVersion);
+			.read(this, packetVersion, packetTypeId);
 	}
 
 	public readInt(length: number): number {
@@ -137,12 +205,13 @@ export const DAY16 = new Day(16, (input) => new Data(input), [
 		const packet = new Reader(data).readPacket();
 		function forEachPacket(packet: APacket, fn: (packet: APacket) => void) {
 			fn(packet);
-			if (packet instanceof OperatorPacket) {
-				packet.subpackets.forEach((subPacket) => forEachPacket(subPacket, fn));
+			if (packet instanceof AOperatorPacket) {
+				packet.subpackets.forEach((subpacket) => forEachPacket(subpacket, fn));
 			}
 		}
 		let sum = 0;
 		forEachPacket(packet, (packet) => (sum += packet.packetVersion));
 		return sum;
 	},
+	(data) => new Reader(data).readPacket().evaluate(),
 ]);
